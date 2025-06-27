934c8e5e5628c76a71d6df241d1a9d999fecc36c
Jayasurya <jayasurya12vj@gmail.com>
2025-06-27 16:36:44 +0530

    fix: remove in succes httpbin

--- routes/success/index.js
+++ routes/success/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   accepted: require('./accepted202'),
   outgoing: require('./outgoingExample'),
-  httpbinMethods: require('./httpbinMethods'),
   delete: require('./delete'),
   put: require('./put-200'),
   post: require('./post-201')
